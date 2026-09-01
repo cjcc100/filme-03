0e7fa3d9e7a96955d6b22e71d81b8d3f4cba0113
cjcc100 <claudineiindiocjcc@gmail.com>
2026-09-01 11:06:41 -0300

    Simplify project to basic version for deploy testing - removed all complex streaming features

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,30 +8,8 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/t/p/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'streamtape.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'tapecontent.net',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'vz-c3b5c7e8-b89.b-cdn.net',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
-  // Adicionar suporte para produção
-  productionBrowserSourceMaps: false,
-  // Otimização de build
-  swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
